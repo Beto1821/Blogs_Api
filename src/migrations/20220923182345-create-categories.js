1c0bc1366ce6169069cb17e631fc1585490b9e8e
Adalberto R. Ribeiro <beto1821@uol.com.br>
2022-09-23 16:51:30 -0300

    req01

---
+++ src/migrations/20220923182345-create-categories.js
@@ -0,0 +1,21 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+     return queryInterface.createTable('categories', {
+     id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+     name: {
+        allowNull: false,
+        type: Sequelize.STRING
+      } 
+      });
+
+  },
+
+  down: (queryInterface, _Sequelize) => queryInterface.dropTable('categories')      
+};
