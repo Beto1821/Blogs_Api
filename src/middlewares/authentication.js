f1e6cd0ec27ef1ba8d3463709c40ef80b2eb7a6e
Adalberto R. Ribeiro <beto1821@uol.com.br>
2022-09-26 14:17:54 +0000

    remove console.log lines

--- src/middlewares/authentication.js
+++ src/middlewares/authentication.js
@@ -15,7 +15,7 @@ module.exports = async (req, res) => {
   if (error) return res.status(400).json({ message: 'Some required fields are missing' });
     
   const user = await User.findOne({ where: { email: req.body.email } }).catch(console.log);
-  console.log(typeof user);
+ 
   if (!user) return res.status(400).json({ message: 'Invalid fields' });
 
   const token = geratoken(req.body.email, req.body.id);
